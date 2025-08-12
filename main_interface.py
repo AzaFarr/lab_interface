e61b5079eb3d1c8480e663492ac86b62b688ca6c
AzaFarr <azaliyafarr@mail.ru>
2025-08-12 23:12:41 +0300

    added label and entry window

--- main_interface.py
+++ main_interface.py
@@ -5,9 +5,6 @@ from func_buttons import *
 from viscometer import *
 
 
-
-
-
 window = Tk()
 
 window.title("labinterface")
@@ -19,16 +16,23 @@ root_btn_f_scr = ButtonMy(btn_f_scr, window)
 btn_f_scr.config(command = root_btn_f_scr.full_screen)
 btn_f_scr.pack(anchor="ne")
 
+
+notebook = ttk.Notebook()
+notebook.pack(fill=BOTH, expand=True)
+
+
 VISCOS = Viscos()
+VISCOS.On()
 
 ELSE_1 = Else_1()
 
 ELSE_2 = Else_2()
 
-notebook = ttk.Notebook()
-notebook.pack(fill=BOTH, expand=True)
+
 notebook.add(VISCOS.get, text="Вискозиметр")
 notebook.add(ELSE_1.get, text="Еще че то")
 notebook.add(ELSE_2.get, text="Еще че то")
+
+
 
 window.mainloop()
--- viscometer.py
+++ viscometer.py
@@ -6,11 +6,26 @@ from func_buttons import ButtonMy
 class Viscos():
 
     def __init__(self):
-        self.VISCOS = Frame(borderwidth=3, relief=RAISED)
+        self.VISCOS = ttk.Frame(borderwidth=3, relief=RAISED)
+
+        self.VISCOS.columnconfigure(index=0, weight=10)
+        self.VISCOS.columnconfigure(index=1, weight=5)
+        self.VISCOS.columnconfigure(index=2, weight=2)
+
+        self.VISCOS.rowconfigure(index=0, weight=1)
+        self.VISCOS.rowconfigure(index=1, weight=1)
+        self.VISCOS.rowconfigure(index=2, weight=1)
+        self.VISCOS.rowconfigure(index=3, weight=10)
 
     def On(self):
-        self.VISCOS.pack(fill=BOTH, expand=True)
 
+        label_1 = ttk.Label(master=self.VISCOS, borderwidth=0, font=("Arial", 20), justify=LEFT, text="Показание с датчика")
+        label_1.grid(column=1, row=1, sticky=SW, padx=0, pady=0)
+
+        enter_value = ttk.Entry(master=self.VISCOS, font=("Arial", 40), justify=LEFT, width=10)
+        enter_value.grid(column=1, row=2, sticky=NW, padx=0, pady=0)
+
+        self.VISCOS.pack(fill=BOTH, expand=True)
 
     @property
     def get(self):
@@ -35,8 +50,6 @@ class Else_2():
 
     def On(self):
         self.ELSE_2.pack(fill=BOTH, expand=True)
-
-
 
     @property
     def get(self):
