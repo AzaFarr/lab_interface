c9281d841a717a67625277c157594b6b5a3c0be3
AzaFarr <azaliyafarr@mail.ru>
2025-08-25 00:53:00 +0300

    че почему осталось

--- main_interface.py
+++ main_interface.py
@@ -1,40 +1,47 @@
-from tkinter import *
-from tkinter import ttk
-
-from func_buttons import *
 from viscometer import *
+from func_buttons import ButtonMy
+
+class Window():
 
 
-window = Tk()
+    def On(self, user_name):
 
-window.title("labinterface")
+        self.WINDOW = Tk()
 
-window.geometry("1000x800")
+        self.WINDOW.title("labinterface")
 
-btn_f_scr = Button(master = window,text = "Полноэкранный режим")
-root_btn_f_scr = ButtonMy(btn_f_scr, window)
-btn_f_scr.config(command = root_btn_f_scr.full_screen)
-btn_f_scr.pack(anchor="ne")
+        self.WINDOW.iconbitmap(default="media/icon.ico")
+
+        self.WINDOW.geometry("1000x800")
+
+        btn_f_scr = Button(master=self.WINDOW, text="Полноэкранный режим")
+        root_btn_f_scr = ButtonMy(btn_f_scr, self.WINDOW)
+        btn_f_scr.config(command=root_btn_f_scr.full_screen)
+        btn_f_scr.pack(anchor="ne")
+
+        label_user = ttk.Label(master=self.WINDOW, text="Пользователь: %s" % user_name)
+        label_user.pack(side=BOTTOM, anchor="e", padx=10)
+
+        notebook = ttk.Notebook(master=self.WINDOW)
+        notebook.pack(fill=BOTH, expand=True)
+
+        VISCOS = Viscos(notebook)
+        VISCOS.On()
+
+        ELSE_1 = Else_1(notebook)
+
+        ELSE_2 = Else_2(notebook)
+
+        notebook.add(VISCOS.get, text="Вискозиметр")
+        notebook.add(ELSE_1.get, text="Еще че то")
+        notebook.add(ELSE_2.get, text="Еще че то")
+
+        self.WINDOW.mainloop()
+
+        print(VISCOS.data)
+        
 
 
-notebook = ttk.Notebook()
-notebook.pack(fill=BOTH, expand=True)
-
-
-VISCOS = Viscos()
-VISCOS.On()
-
-ELSE_1 = Else_1()
-
-ELSE_2 = Else_2()
-
-
-notebook.add(VISCOS.get, text="Вискозиметр")
-notebook.add(ELSE_1.get, text="Еще че то")
-notebook.add(ELSE_2.get, text="Еще че то")
 
 
 
-window.mainloop()
-
-print(VISCOS.data)
