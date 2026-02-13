048ab9462428a0dc950a93bea32c0d9d0d51d0f5
AzaFarr <azaliyafarr@mail.ru>
2026-02-13 15:22:55 +0500

    common framework is created

--- main_components/load_ui.py
+++ main_components/load_ui.py
@@ -1,6 +1,12 @@
 import numpy as np
 from PyQt5 import QtWidgets, uic
 
+#TODO: make fullscreen as default mode
+#TODO: change the HTML codes (they in TextBrowsers)
+#TODO: improove common styles
+#TODO: connect buttons with func
+#TODO: finish the table with data
+
 
 class MainWindow(QtWidgets.QMainWindow):
 
