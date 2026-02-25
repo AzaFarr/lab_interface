630558265b50bbd3e794dcd59c827afc7fd2b6be
AzaFarr <azaliyafarr@mail.ru>
2026-02-25 09:30:48 +0500

    добавила тудушку

--- main_components/funcs_button.py
+++ main_components/funcs_button.py
@@ -1,9 +1,8 @@
 from PyQt5 import QtWidgets, QtGui
 
+#TODO: убрать первый столдец с нумерацией, который автоматически возникает
+
 def add_item(model: QtGui.QStandardItemModel):
+    model.appendRow([QtGui.QStandardItem(str(i)) for i in range(3)])
 
-    def command():
-        model.appendRow([QtGui.QStandardItem(str(i)) for i in range(3)])
-
-    return command()
 
