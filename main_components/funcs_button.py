f5f4ce3468f98a21f22e629bbd32ce384bae0018
AzaFarr <azaliyafarr@mail.ru>
2026-03-03 09:27:55 +0500

    фикс

--- main_components/funcs_button.py
+++ main_components/funcs_button.py
@@ -4,7 +4,6 @@ import datetime as dt
 
 
 def add_item(tabModel: Model, lineEdit: str):
-    print('lineEdit_6 func started')
     number = QtGui.QStandardItem(str(tabModel.i))
     exp_data = QtGui.QStandardItem(lineEdit)
     date = QtGui.QStandardItem(str(dt.datetime.now()))
